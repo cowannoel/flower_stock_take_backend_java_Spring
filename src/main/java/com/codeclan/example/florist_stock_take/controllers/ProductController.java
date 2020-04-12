850e77c533398f34c0c6cb83a65f91b99e69d822
djghis <djghis@hotmail.com>
2020-04-12 18:09:39 +0100

    same routes added for products and tested n insomnia

--- src/main/java/com/codeclan/example/florist_stock_take/controllers/ProductController.java
+++ src/main/java/com/codeclan/example/florist_stock_take/controllers/ProductController.java
@@ -1,13 +1,11 @@
 package com.codeclan.example.florist_stock_take.controllers;
 
+import com.codeclan.example.florist_stock_take.models.Product;
 import com.codeclan.example.florist_stock_take.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -19,6 +17,29 @@ public class ProductController {
     @GetMapping
     public ResponseEntity getAllProducts(){
         return new ResponseEntity(productRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product>getProduct(@PathVariable Long id){
+        return new ResponseEntity(productRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product>postProduct(@RequestBody Product product){
+        productRepository.save(product);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Product> putProduct(@RequestBody Product product){
+        productRepository.save(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteProduct(@PathVariable Long id){
+        productRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
     @GetMapping(value = "/{name}")
